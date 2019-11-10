1e3e840051036ff7da3b091bbf52b2798b6d4837
Mark Mossberg <mark.mossberg@gmail.com>
2019-11-09 18:20:52 -0800

    Support checking individual sample files
    Refactor cli to take either dir or filename.

--- tools/check_drumkit.py
+++ tools/check_drumkit.py
@@ -1,4 +1,5 @@
 import logging
+import os.path
 logging.basicConfig(level=logging.DEBUG)
 
 from argparse import ArgumentParser
@@ -8,7 +9,7 @@ from pyableton import AbletonCollection
 
 def get_args():
     p = ArgumentParser(description='Check what samples in a drumkit are used in all your ableton projects')
-    p.add_argument('--drumkit', help='Path to drumkit', required=True)
+    p.add_argument('path', help='Path to sample file or drumkit folder')
     p.add_argument('--collection', help='(e.g. ~/Music/Ableton)', default='~/Music/Ableton')
     return p.parse_args()
 
@@ -22,14 +23,22 @@ def main():
         print(f'{args.collection} not found')
         return
 
-    usage = collection.check_dir_used(args.drumkit)
+    if os.path.isdir(args.path):
+        usage = collection.check_dir_used(args.path)
 
-    for file, usa in usage.items():
-        print(file)
+        for file, usa in usage.items():
+            print(file)
+            for proj, sets in usa.items():
+                print('\t', proj)
+                for set in sets:
+                    print('\t\t', set)
+    else:
+        usa = collection.check_file_used(args.path)
         for proj, sets in usa.items():
-            print('\t', proj)
+            print(proj)
             for set in sets:
-                print('\t\t', set)
+                print('\t', set)
+
 
 if __name__ == '__main__':
     main()
